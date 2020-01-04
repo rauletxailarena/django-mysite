1804349b389e0f32df4cdf81fbee51f458be2ad7
Raul Ruiz <raul.ruiz@ebury.com>
2020-01-04 19:45:34 +0100

    blog project finished

--- blog/urls.py
+++ blog/urls.py
@@ -5,8 +5,9 @@ app_name = 'blog'
 
 urlpatterns = [
     # post views
-    path('', views.post_list, name='post_list'),
-    path('<int:year>/<int:month>/<int:day>/<slug:post>/',
+    # path('', views.post_list, name='post_list'),
+    path('', views.PostListView.as_view(), name='post_list'),
+    path('<int:year>/<int:month>/<int:day>/<slug:post_slug>/',
          views.post_detail,
          name='post_detail'),
 ]
